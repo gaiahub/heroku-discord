fb60f4553817857e1289c5f2268710aca246c0b7
Rui Coelho <ruicoelho@ua.pt>
2024-01-14 23:58:24 +0000

    Update webhook structure

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const webhook = process.env.DISCORD_WEBHOOK;
+
+console.log(webhook)
 
 app.use(bodyParser.json());
 
 app.post('/heroku-webhook', (req, res) => {
   // Extract relevant data from Heroku webhook payload
-  const herokuEventData = req.body;
+  const herokuEventData = req.body.data;
+  const herokuEventMetada = req.body.webhook_metadata;
+
+
+  const message = `Event ID => ${herokuEventMetada.event.id}\nEvent Type => ${herokuEventMetada.event.include}\nTriggered by => ${herokuEventData.user.email}\nStatus => ${herokuEventData.status}`
+
 
   // Transform data to match Discord webhook payload structure
   const discordPayload = {
-    content: `Heroku Event: ${herokuEventData.event}`,
     embeds: [
       {
         title: 'Heroku Notification',
-        description: 'Details about the event...',
+        description: message,
         fields: [
           { name: 'App Name', value: herokuEventData.app.name, inline: true },
           // Add more fields as needed
@@ -27,7 +34,7 @@ app.post('/heroku-webhook', (req, res) => {
   };
 
   // Send payload to Discord webhook
-  axios.post(`${process.env.DISCORD_WEBHOOK}`, discordPayload)
+  axios.post(`${webhook}`, discordPayload)
     .then(() => res.sendStatus(200))
     .catch(error => {
       console.error('Error sending Discord webhook:', error);
